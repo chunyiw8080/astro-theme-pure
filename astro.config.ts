f9cd8f5531f724cf04b9ea982c66c34e75c6194f
ChunyiWang <83103931+chunyiw8080@users.noreply.github.com>
2025-12-14 16:31:02 +1100

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -25,7 +25,7 @@ import config from './src/site.config.ts'
 // https://astro.build/config
 export default defineConfig({
   // [Basic]
-  site: 'astro-theme-pure-6ha.pages.dev',
+  site: 'https://astro-theme-pure-6ha.pages.dev',
   // Deploy to a sub path
   // https://astro-pure.js.org/docs/setup/deployment#platform-with-base-path
   // base: '/astro-pure/',
